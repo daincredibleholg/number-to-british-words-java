7003eb5fb9c476da390ce59a30be9455bbb31a83
Holger Steinhauer <holger+github@willige-entwickler.de>
2015-07-05 18:42:32 +0100

    Added argument size check

--- src/main/java/net/h0lg/java/tests/number2words/input/ArgumentValidator.java
+++ src/main/java/net/h0lg/java/tests/number2words/input/ArgumentValidator.java
@@ -1,11 +1,33 @@
 package net.h0lg.java.tests.number2words.input;
 
 /**
- * Created by holgersteinhauer on 05/07/15.
+ * This class will check a given String array if
+ *   a) it only contains one element and
+ *   b) the only element is an integer between 0 and 999,999,999.
+ * If both requirements are met, the main method process() returns a UserInput instance
+ * with the fount int value. Otherwise the value will in the UserInput instance will -1
+ * and the faulty flag is set.
  */
 public class ArgumentValidator {
 
+    private static final int FAULTY_DEFAULT_VALUE = -1;
+
     public static UserInput process(String[] arguments) {
-        return new UserInput(2, false);
+
+        try {
+            checkArguments(arguments);
+            int value = Integer.parseInt(arguments[0]);
+            return new UserInput(value, false);
+        } catch (IllegalArgumentException e) {
+            return new UserInput(FAULTY_DEFAULT_VALUE, true);
+        }
     }
+
+    private static void checkArguments(String[] arguments) throws IllegalArgumentException {
+        if (arguments == null || arguments.length > 1) {
+            throw new IllegalArgumentException("Only exact one argument is supported.");
+        }
+    }
+
+
 }
--- src/test/java/net/h0lg/java/tests/number2words/input/ArgumentValidatorTest.java
+++ src/test/java/net/h0lg/java/tests/number2words/input/ArgumentValidatorTest.java
@@ -21,4 +21,26 @@ public class ArgumentValidatorTest {
         Assert.assertEquals(expectedInput, actualInput);
     }
 
+    @Test
+    public void anotherValidInputIsParsed () {
+        int validInputValue = 42;
+        boolean expectedFaultyState = false;
+
+        String[] validInput = { Integer.toString(validInputValue) };
+        UserInput expectedInput = new UserInput(validInputValue, expectedFaultyState);
+
+        UserInput actualInput = ArgumentValidator.process(validInput);
+
+        Assert.assertEquals(expectedInput, actualInput);
+    }
+
+    @Test
+    public void moreThanOneArgumentReturnsInputWithFaultyFlagSet () {
+        int validInputValue = 42;
+
+        String[] invalidInput = { Integer.toString(validInputValue), Integer.toString(validInputValue) };
+        UserInput actualInput = ArgumentValidator.process(invalidInput);
+
+        Assert.assertTrue(actualInput.isFaulty());
+    }
 }
