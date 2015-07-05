87c6046437dde6696b9d5dbc29ef0071d64335ef
Holger Steinhauer <holger+github@willige-entwickler.de>
2015-07-05 16:35:54 +0100

    * Corrected JUnit version in generated POM file and added Java 8 as source and target version * Introduced console abstraction as using Java's Console feature is breaking usage in IDEs and automated testing.

--- src/test/java/net/h0lg/java/tests/text_device/TextDeviceFactoryTest.java
+++ src/test/java/net/h0lg/java/tests/text_device/TextDeviceFactoryTest.java
@@ -4,14 +4,29 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Testing the factory for console abstraction.
+ * Tests for static factory method to create a appropriate TextDevice instance.
  *
  */
 public class TextDeviceFactoryTest {
 
     @Test
-    public void createTextDevice() throws Exception{
+    public void createTextDevice() throws Exception {
         TextDevice textDevice = TextDeviceFactory.createTextDevice();
         Assert.assertNotNull(textDevice);
+    }
+
+    @Test
+    public void characterTextDeviceShouldBeDefaultInTests() throws Exception {
+        TextDevice textDevice = TextDeviceFactory.createTextDevice();
+
+        Assert.assertTrue(textDevice instanceof CharacterTextDevice);
+    }
+
+    @Test
+    public void checkInstanceIsOnlyCreatedOnce () throws Exception {
+        TextDevice textDevice1 = TextDeviceFactory.createTextDevice();
+        TextDevice textDevice2 = TextDeviceFactory.createTextDevice();
+
+        Assert.assertTrue(textDevice1 == textDevice2);
     }
 }
