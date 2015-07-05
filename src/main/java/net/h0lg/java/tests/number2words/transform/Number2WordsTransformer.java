c2d10f5084a1edd65b17878efafc28843b9fe779
Holger Steinhauer <holger+github@willige-entwickler.de>
2015-07-05 19:29:49 +0100

    Introduced Number2WordsTransformer with corresponding test class

---
+++ src/main/java/net/h0lg/java/tests/number2words/transform/Number2WordsTransformer.java
@@ -0,0 +1,11 @@
+package net.h0lg.java.tests.number2words.transform;
+
+/**
+ * Created by holgersteinhauer on 05/07/15.
+ */
+public class Number2WordsTransformer {
+
+    public static String transform(int value) {
+        return "two";
+    }
+}
---
+++ src/test/java/net/h0lg/java/tests/number2words/transform/Number2WordsTransformerTest.java
@@ -0,0 +1,21 @@
+package net.h0lg.java.tests.number2words.transform;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by holgersteinhauer on 05/07/15.
+ */
+public class Number2WordsTransformerTest {
+
+    @Test
+    public void smallPositiveIntegerGetsTransformed () {
+        int validInput = 2;
+        String expected = "two";
+
+        String actual = Number2WordsTransformer.transform(validInput);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+}
