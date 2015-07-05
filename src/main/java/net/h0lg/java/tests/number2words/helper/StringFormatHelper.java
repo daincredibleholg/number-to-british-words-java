91109bdb4eaed4dd676ea71058b7a6166734d36e
Holger Steinhauer <holger+github@willige-entwickler.de>
2015-07-05 21:04:14 +0100

    Added documentation to helper class.

--- src/main/java/net/h0lg/java/tests/number2words/helper/StringFormatHelper.java
+++ src/main/java/net/h0lg/java/tests/number2words/helper/StringFormatHelper.java
@@ -1,10 +1,20 @@
 package net.h0lg.java.tests.number2words.helper;
 
 /**
- * Created by holgersteinhauer on 05/07/15.
+ * This helper class provides methods to manipulate String null safe.
+ *
  */
 public class StringFormatHelper {
 
+    /**
+     * Adds stringToAdd at the beginning of value if value is
+     *  a) not null and
+     *  b) not empty
+     *
+     * @param value The value you need another string in front of.
+     * @param stringToAdd The String to add at value's front.
+     * @return The combination stringToAdd and value or value, if the above requirements are not met.
+     */
     public static String addBeforeIfNotEmpty(String value, String stringToAdd) {
         if (value == null || value.isEmpty()) {
             return value;
