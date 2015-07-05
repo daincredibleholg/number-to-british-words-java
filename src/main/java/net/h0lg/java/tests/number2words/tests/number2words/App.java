dc87b463076a7d3008508edbdbc70ba356ff4cc9
Holger Steinhauer <holger+github@willige-entwickler.de>
2015-07-05 17:59:22 +0100

    Back to start, will simplify the application. Therefore I removed the existing code.

---
+++ src/main/java/net/h0lg/java/tests/number2words/tests/number2words/App.java
@@ -0,0 +1,19 @@
+package net.h0lg.java.tests.number2words.tests.number2words;
+
+
+/**
+ * This class is just the entry point for the application
+ *
+ */
+public class App 
+{
+
+    public static void main( String[] args )
+    {
+        App app = new App();
+        app.start();
+    }
+
+    private void start() {
+    }
+}
